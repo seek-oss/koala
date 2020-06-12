226a8b347c03a95b00d75654bc7b1eb446213692
Ryan Ling <ryan@outlook.com.au>
2020-06-12 11:39:16 +1000

    fix(ErrorMiddleware): Export from index (#11)
    🤦🏻‍♂️

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as AsyncMiddleware from './asyncMiddleware/asyncMiddleware';
+export * as ErrorMiddleware from './errorMiddleware/errorMiddleware';
 export * as MetricsMiddleware from './metricsMiddleware/metricsMiddleware';
 export * as RequestLogging from './requestLogging/requestLogging';
 export * as SecureHeaders from './secureHeaders/secureHeaders';
