733cbd57abc63dcd279526b3666020754bace765
Sam Chung <samc@seek.com.au>
2022-05-27 10:22:38 +1000

    fix(RequestLogging.createContextStorage): Shallow copy context store for the `mixin` function (#155)

--- src/requestLogging/requestLogging.ts
+++ src/requestLogging/requestLogging.ts
@@ -193,8 +193,8 @@ export const createContextStorage = () => {
         await loggerContext.run(getFieldsFn(ctx, contextFields(ctx)), next);
       },
     /**
-     * Returns fields from the logger context store
+     * Returns a shallow copy of fields from the logger context store. For performance reason we only copy the surface level fields.
      */
-    mixin: () => loggerContext.getStore() ?? {},
+    mixin: () => ({ ...loggerContext.getStore() }),
   };
 };
